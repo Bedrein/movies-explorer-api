bc96ce95c59a1409aed05de055dd4646681ef61d
bedrein <dimabedrein118@gmail.com>
2023-09-21 23:36:01 +0300

    deploy_1_3

--- app.js
+++ app.js
@@ -1,20 +1,31 @@
-require('dotenv').config();
-const express = require('express');
-const mongoose = require('mongoose');
-const bodyParser = require('body-parser');
-const { errors } = require('celebrate');
-const helmet = require('helmet');
-const cors = require('cors');
-const { requestLogger, errorLogger } = require('./middlewares/logger');
-const router = require('./routes');
-const errorHandler = require('./middlewares/errorHandler');
-const { PORT, MONGO_DB } = require('./utils/constants');
+require("dotenv").config();
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const { errors } = require("celebrate");
+const helmet = require("helmet");
+const cors = require("cors");
+const { requestLogger, errorLogger } = require("./middlewares/logger");
+const router = require("./routes");
+const errorHandler = require("./middlewares/errorHandler");
+const { PORT, MONGO_DB } = require("./utils/constants");
 
 mongoose.connect(MONGO_DB);
 
 const app = express();
 
-app.use(cors({ origin: ['http://api.bedrein-movies.nomoredomainsicu.ru', 'https://api.bedrein-movies.nomoredomainsicu.ru', 'http://localhost:3000', 'http://localhost:3001'] }));
+app.use(
+  cors({
+    origin: [
+      "http://api.bedrein-movies.nomoredomainsicu.ru",
+      "https://api.bedrein-movies.nomoredomainsicu.ru",
+      "http://localhost:3000",
+      "http://localhost:3001",
+      "http://bedrein-movies.nomoredomainsicu.ru",
+      "https://bedrein-movies.nomoredomainsicu.ru",
+    ],
+  })
+);
 
 app.use(helmet());
 
